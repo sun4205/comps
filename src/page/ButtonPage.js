6735c5971f96bf6b5b5919f532d48b57884bac09
sun4205 <hanna4205@gmail.com>
2025-08-13 18:53:16 -0400

    organize files

--- src/page/ButtonPage.js
+++ src/page/ButtonPage.js
@@ -1,7 +1,7 @@
 import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
-import Button from "./components/Button";
+import Button from "../components/Button";
 
-function App() {
+function ButtonPage() {
   const handleClick = () => {
     console.log("click");
   };
@@ -39,4 +39,4 @@ function App() {
   );
 }
 
-export default App;
+export default ButtonPage;
